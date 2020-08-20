60b218a417334abf1599f086819f79739f995e0d
Jelly-code <santamaria.javiera@gmail.com>
2020-08-20 12:37:07 -0400

    javascript condicional

---
+++ main.js
@@ -0,0 +1,13 @@
+const puedeVerPelicula = (edad, tieneAutorizacion) => {
+    if (edad >= 15 || tieneAutorizacion) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+console.log(puedeVerPelicula(12, false))
+console.log(puedeVerPelicula(12, true))
+console.log(puedeVerPelicula(16, false))
+console.log(puedeVerPelicula(38, true))
