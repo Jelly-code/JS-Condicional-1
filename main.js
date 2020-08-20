e8cd86969b4d599d2ed3dafade2c3204734f3795
Jelly-code <santamaria.javiera@gmail.com>
2020-08-20 12:41:44 -0400

    ejercicio 2

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// FUNCIONES, PARAMETROS Y CONDICIONES
+// EJERCICIO 1
+
 const puedeVerPelicula = (edad, tieneAutorizacion) => {
     if (edad >= 15 || tieneAutorizacion) {
         return true
@@ -11,3 +14,25 @@ console.log(puedeVerPelicula(12, false))
 console.log(puedeVerPelicula(12, true))
 console.log(puedeVerPelicula(16, false))
 console.log(puedeVerPelicula(38, true))
+
+// EJERCICIO 2
+
+const avanzarSemaforo = (colorActual) => {
+    if (colorActual == 'verde') {
+         return 'amarillo'
+    }
+    else if (colorActual == 'amarillo') {
+         return 'rojo'
+    }
+    else if (colorActual == 'rojo'){
+         return 'verde'
+    }
+    else {
+        return 'Error: color de semáforo inválido'
+    } 
+ }
+ 
+ console.log(avanzarSemaforo('verde'))
+ console.log(avanzarSemaforo('amarillo'))
+ console.log(avanzarSemaforo('rojo'))
+ console.log(avanzarSemaforo('lila'))
